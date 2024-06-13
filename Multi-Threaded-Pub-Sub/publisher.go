f6dee4664afb84e6f7da2462c652e28dc2b90651
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-06-13 18:12:30 +0530

    Update publisher.go

--- Multi-Threaded-Pub-Sub/publisher.go
+++ Multi-Threaded-Pub-Sub/publisher.go
@@ -2,15 +2,21 @@ package main
 
 import "sync"
 
-type Publisher struct {
+type publisher struct {
 	Name string
 }
 
-func NewPublisher(name string) *Publisher {
-	return &Publisher{Name: name}
+type Publisher interface {
+	Publish(message string, topic *Topic, wg *sync.WaitGroup)
 }
 
-func (p *Publisher) publish(message string, channel chan<- string, wg *sync.WaitGroup) {
+func NewPublisher(name string) Publisher {
+	return &publisher{Name: name}
+}
+
+func (p *publisher) Publish(message string, topic *Topic, wg *sync.WaitGroup) {
 	defer wg.Done()
-	channel <- message + "pub" + p.Name
+	for _, channel := range topic.Channels {
+		channel <- message + "->publisher" + p.Name
+	}
 }
